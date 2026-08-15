2e0cb653c9bc6dc346dcee48f1a2d4bfa2910508
verdespastosagro-byte <verdespastos.agro@gmail.com>
2026-08-15 04:10:41 +0000

    Renomear aplicativo para Deuteronômio 28

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,13 +1,34 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
+
+
 const config: CapacitorConfig = {
+  
   appId: 'com.ccbcultos.integrado',
-  appName: 'Cultos CCB',
+  
+  appName: 'Deuteronômio 28',
+  
   webDir: 'www',
+  
   server: {
+    
     url: 'https://preview--culto-insight-hub.lovable.app',
+    
     cleartext: false
+      
   }
+    
 };
 
+
+
 export default config;
+
+
+
+
+
+
+
+
+
